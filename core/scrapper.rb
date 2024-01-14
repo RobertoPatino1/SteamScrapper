01089899ccfd16a5c25b9eab0128684028c45ac4
Roberto Patiño <rapatino@espol.edu.ec>
2024-01-14 17:16:32 -0500

    Finished games extraction

--- core/scrapper.rb
+++ core/scrapper.rb
@@ -1,42 +1,54 @@
 require 'open-uri'
 require 'nokogiri'
-require 'csv'
 require './core/file_handler'
-class Scrapper
 
+class Scrapper
     attr_accessor :url
 
     def initialize(url)
         @url = url
     end
-
+    
     def extract_games
-        File_Handler.create_file("games.csv",["Titulo","Fecha de lanzamiento","Precio", "Plataformas"])
-        steam_html = URI.open(@url)
-        data = steam_html.read
-        parsed_content = Nokogiri::HTML(data)
+        File_Handler.create_file("games.csv", ["Titulo", "Fecha de lanzamiento", "Precio", "Plataformas"])
+        page = 1
+        total_lines = 100
 
-        parsed_content.css('#search_resultsRows a.search_result_row').each do |game|
-        title = game.css('.search_name .title').text.strip
-        release_date = game.css('.search_released').text.strip
-        price = game.css('.search_price_discount_combined .discount_final_price').text.strip
+        loop do
+            page_url = "#{@url}?page=#{page}"
+            steam_html = URI.open(page_url)
+            data = steam_html.read
+            parsed_content = Nokogiri::HTML(data)
 
-        platform_info = extract_platform_info(game)
+            games = parsed_content.css('#search_resultsRows a.search_result_row')
 
-        puts "#{title} - #{release_date} - #{price} - Plataformas: #{platform_info}"
-        File_Handler.write_to_file('games.csv',[title,release_date,price,platform_info])
+            break if games.empty?
+
+            games.each do |game|
+                title = game.css('.search_name .title').text.strip
+                release_date = game.css('.search_released').text.strip
+                price = game.css('.search_price_discount_combined .discount_final_price').text.strip
+
+                platform_info = extract_platform_info(game)
+                
+                puts "#{title} - #{release_date} - #{price} - #{platform_info}"
+                File_Handler.write_to_file('games.csv', [title, release_date, price, platform_info])
+                
+                total_lines -= 1
+                break if total_lines <= 0
+            end
+            break if total_lines <= 0
+            page += 1
         end
     end
+
 
     def extract_platform_info(game)
         platform_span = game.css('.search_name .platform_img')
         
         platform_info = platform_span.map do |span|
-        span['class'].split.last 
+        span['class'].split.last
         end.join('-')
-
         platform_info.empty? ? 'No disponible' : platform_info
     end
-
-
 end
