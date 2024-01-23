8f9d68b87d5136f29a117b0f57c02c3abb552d99
DanielMateoOrellana <107244270+DanielMateoOrellana@users.noreply.github.com>
2024-01-22 22:46:41 -0500

    Added last main changes (Questions 7, 8, 9)

--- main.rb
+++ main.rb
@@ -10,9 +10,18 @@ MAIN_URL = 'https://store.steampowered.com/search/'
 scrapper = Scrapper.new(MAIN_URL)
 scrapper.extract_games()
 
-scrapper.extraer_codigos_categorias()          
+scrapper.extraer_codigos_categorias()
+
+scrapper.count_vr_games_to_csv
+
+languages = ['english', 'simplifiedchinese', 'traditionalchinese', 'japanese', 'korean', 'thai', 'bulgarian', 'czech', 'danish', 'german', 'spanish', 'spanishlatinamerica', 'greek', 'french', 'italian', 'indonesian']
+
+scrapper.count_games_by_language_to_csv(languages)
 
 categoria = 'atmospheric'
 
 scrapper.extraer_titulos_categoria(categoria)
 
+scrapper2 = Scrapper.new("https://store.steampowered.com/search/?specials=1&ndl=1")
+
+scrapper2.extract_game_prices
